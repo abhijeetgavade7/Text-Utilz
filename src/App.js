e66d3c4baaca5a9e65de414a901f1c56fefe30ef
abhijeetgavade7 <abhijeetgavade7777@gmail.com>
2024-12-29 20:27:21 +0530

    Text utilz

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import React, {useActionState, useState}  from 'react'
-
-
+import About from './components/About';
+// import {
+//   BrowserRouter as Router,
+//   Routes, // Use Routes instead of Switch
+//   Route, // Route remains the same but with a new syntax
+// } from "react-router-dom";
 
 function App() {
-   const [mode, setmode] = useState('light');
-   const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-   const showAlert =(massage,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-     msg:massage,
-    type:type
-    }) 
+      msg: message,
+      type: type
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-   }
-   
+  };
 
-  const togglemode = ()=>{
-    if(mode==='dark')
-    {
-      setmode('light');
-      document.body.style.backgroundColor="white";
-     showAlert("Dark mode is off","success")
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      showAlert("Dark mode is off", "success");
+    } else {
+      setMode('dark');
+      document.body.style.backgroundColor = "#F0F8FF";
+      showAlert("Dark mode is on", "success");
     }
-    else
-      {
-        setmode('dark');
-        document.body.style.backgroundColor="#F0F8FF";
-        showAlert("Dark mode is on","success")
-      }
+  };
 
-  }
   return (
     <>
-      <Navbar title="TextUtilz" Home="Home" mode={mode} togglemode={togglemode}/>
-    <Alert alert={alert}/> 
-      {/* <About/> */}
-      <TextForm showAlert={showAlert}/>
+     
+        <Navbar title="TextUtilz" Home="Home" mode={mode} togglemode={toggleMode} />
+        <Alert alert={alert} />
+        <TextForm showAlert={showAlert} mode={mode} heading="Enter the text here" />
       
+        {/* <Router>
+        
+          <Routes>
+            <Route exact path="/" element={ } />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+      
+      </Router> */}
     </>
   );
 }
 
-export default App;  
+export default App;
